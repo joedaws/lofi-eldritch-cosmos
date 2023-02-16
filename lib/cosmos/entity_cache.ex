b3d6b7a9dc096bf9e3ded4ae7517edfd53a241fb
Joseph Daws <daws.joseph@gmail.com>
2023-02-15 23:48:04 -0500

    added server and cache for entities

---
+++ lib/cosmos/entity_cache.ex
@@ -0,0 +1,28 @@
+defmodule Cosmos.EntityCache do
+  use GenServer
+
+  def start() do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def server_process(cache_pid, entity_id) do
+    GenServer.call(cache_pid, {:server_process, entity_id})
+  end
+
+  @impl true
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  @impl true
+  def handle_call({:server_process, entity_id}, _from, entity_servers) do
+    case Map.fetch(entity_servers, entity_id) do
+      {:ok, entity_server} ->
+        {:reply, entity_server, entity_servers}
+
+      :error ->
+        {:ok, new_server} = Cosmos.EntityServer.start()
+        {:reply, new_server, Map.put(entity_servers, entity_id, new_server)}
+    end
+  end
+end
---
+++ lib/cosmos/entity_server.ex
@@ -0,0 +1,44 @@
+defmodule Cosmos.EntityServer do
+  use GenServer
+
+  @doc """
+  While flushing out the functionality we naively have a single Entity Server.
+  This will be changed in the future
+  """
+  def start() do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def get(entity_server) do
+    GenServer.call(entity_server, {:get})
+  end
+
+  def add_component(entity_server, component) do
+    GenServer.cast(entity_server, {:add_component, component})
+  end
+
+  def delete_component(entity_server, component_id) do
+    GenServer.cast(entity_server, {:delete_component, component_id})
+  end
+
+  @impl true
+  def init(_) do
+    new_entity = Cosmos.Entity.new()
+    {:ok, new_entity}
+  end
+
+  @impl true
+  def handle_call({:get}, _from, entity) do
+    {:reply, entity, entity}
+  end
+
+  @impl true
+  def handle_cast({:add_component, component}, entity) do
+    {:noreply, Cosmos.Entity.add_component(entity, component)}
+  end
+
+  @impl true
+  def handle_cast({:delete_component, component_id}, entity) do
+    {:noreploy, Cosmos.Entity.delete_component(entity, component_id)}
+  end
+end
